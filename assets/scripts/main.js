77caa36a395b158812f45326dfb9d9e57b43418a
Cathyyyy123 <changs0604@gmail.com>
2023-05-21 19:00:03 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -48,7 +48,7 @@ function initializeServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
       try {
-        const registration = navigator.serviceWorker.register("/sw.js");
+        const registration = navigator.serviceWorker.register("/sw.js", {scope: "./",});
         console.log("Registration successful");
       } catch (error) {
         console.error(`The service worker registration fails with ${error}`);
